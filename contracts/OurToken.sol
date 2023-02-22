fd1969b7ac4f1238f62a58e0f92fba1bddd14926
pratokko <pratokko3@gmail.com>
2023-02-22 11:20:40 +0300

    first commit

---
+++ contracts/OurToken.sol
@@ -0,0 +1,11 @@
+//SPDX-Lisence-Identifier: MIT
+pragma solidity ^0.8.7;
+
+    import "@openzepplin/contracts/token/ERC20/ERC20.sol";
+
+contract OurToken is ERC20{
+   
+    constructor( uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
